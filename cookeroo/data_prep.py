d0a2b33bb5eb5ccdc2db0a454cc10382792f9676
Abhilash Kishore <abhilash1in@gmail.com>
2020-03-31 23:22:43 -0700

    Refactor non-public methods

--- cookeroo/data_prep.py
+++ cookeroo/data_prep.py
@@ -13,7 +13,7 @@ class DataPrep():
     def __init__(self):
         super().__init__()
 
-    def get_file_paths(self, folder_path, extension):
+    def _get_file_paths(self, folder_path, extension):
         file_paths = []
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
@@ -21,7 +21,7 @@ class DataPrep():
                 file_paths.append(file_path)
         return file_paths
 
-    def get_audio_segments_wav(self, wav_file_paths):
+    def _get_audio_segments_wav(self, wav_file_paths):
         audio_segments = []
         for file_path in wav_file_paths:
             if file_path.endswith(file_path):
@@ -29,15 +29,15 @@ class DataPrep():
                 audio_segments.append(audio_segment)
         return audio_segments
 
-    def get_sliced_audo_segments(self, audio_segments, slice_interval, allow_gaps=True):
+    def _get_sliced_audio_segments(self, audio_segments, slice_interval, allow_gaps=True):
         res_sliced_audo_segments = []
         for audio_segment in audio_segments:
             # print(audio_segment_tuple[0] + ' ' + str(len(get_sliced_audio_segments(audio_segment_tuple, slice_interval))))
-            sliced_audio_segments = self.get_sliced_audio_segments(audio_segment, slice_interval, allow_gaps)
+            sliced_audio_segments = self._slice_audio_segment(audio_segment, slice_interval, allow_gaps)
             res_sliced_audo_segments.extend(sliced_audio_segments)
         return res_sliced_audo_segments
 
-    def get_sliced_audio_segments(self, audio_segment, slice_interval, allow_gaps=True):
+    def _slice_audio_segment(self, audio_segment, slice_interval, allow_gaps=True):
         sliced_audio_segments = []
         start = 0
         while start < len(audio_segment):
@@ -52,18 +52,18 @@ class DataPrep():
             start = end
         return sliced_audio_segments
 
-    def export_audio_segments(self, audio_segments, folder_path, extension):
+    def _export_audio_segments(self, audio_segments, folder_path, extension):
         for index, sliced_audo_segment in enumerate(audio_segments):
             sliced_audo_segment.export(
                 os.path.join(folder_path, '{filename}.{extension}'.format(filename=index, extension=extension)),
                 format=extension)
 
-    def clear_dir(self, folder_path):
+    def _clear_dir(self, folder_path):
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 os.remove(os.path.join(root, file))
 
-    def extract_features(self, file_path):
+    def _extract_features(self, file_path):
         try:
             audio, sample_rate = librosa.load(file_path, res_type='kaiser_fast')
             mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
@@ -95,13 +95,13 @@ class DataPrep():
             # created the sliced folder if it does not exist
             Path(category_data_sliced_folder_path).mkdir(parents=True, exist_ok=True)
             # clear old data in sliced folder
-            self.clear_dir(category_data_sliced_folder_path)
+            self._clear_dir(category_data_sliced_folder_path)
 
             # get list of file paths with given extension
-            category_data_raw_file_paths = self.get_file_paths(category_data_raw_folder_path, extension)
+            category_data_raw_file_paths = self._get_file_paths(category_data_raw_folder_path, extension)
             # list of AudioSegments (one for each file)
-            audio_segments = self.get_audio_segments_wav(category_data_raw_file_paths)
+            audio_segments = self._get_audio_segments_wav(category_data_raw_file_paths)
             # list of all sliced AudioSegments combined
-            sliced_audo_segments = self.get_sliced_audo_segments(audio_segments, slice_interval)
+            sliced_audo_segments = self._get_sliced_audo_segments(audio_segments, slice_interval)
             # save sliced AudioSegments to sliced folder
-            self.export_audio_segments(sliced_audo_segments, category_data_sliced_folder_path, extension)
+            self._export_audio_segments(sliced_audo_segments, category_data_sliced_folder_path, extension)
