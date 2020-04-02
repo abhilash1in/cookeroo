2d81248698a015c3365523c323ce9a70aaf317e9
Abhilash Kishore <abhilash1in@gmail.com>
2020-04-01 19:33:34 -0700

    Separate slice and export

--- cookeroo/data_prep.py
+++ cookeroo/data_prep.py
@@ -10,8 +10,16 @@ import numpy as np
 
 
 class DataPrep():
-    def __init__(self):
+    def __init__(self, data_base_path, extension):
         super().__init__()
+        # data_base_path = '/Users/abhilash1in/Documents/Projects/Cookeroo/data/'
+        # extension = 'wav'
+        self._data_base_path = data_base_path
+        self._extension = extension
+
+        self._raw_data_path = os.path.join(self._data_base_path, 'raw')
+        self._sliced_data_path = os.path.join(self._data_base_path, 'sliced')
+        self._sliced_audio_segments = {}
 
     def _get_file_paths(self, directory_path, extension):
         file_paths = []
@@ -30,12 +38,12 @@ class DataPrep():
         return audio_segments
 
     def _get_sliced_audio_segments(self, audio_segments, slice_interval, allow_gaps=True):
-        res_sliced_audo_segments = []
+        res_sliced_audio_segments = []
         for audio_segment in audio_segments:
             # print(audio_segment_tuple[0] + ' ' + str(len(get_sliced_audio_segments(audio_segment_tuple, slice_interval))))
             sliced_audio_segments = self._slice_audio_segment(audio_segment, slice_interval, allow_gaps)
-            res_sliced_audo_segments.extend(sliced_audio_segments)
-        return res_sliced_audo_segments
+            res_sliced_audio_segments.extend(sliced_audio_segments)
+        return res_sliced_audio_segments
 
     def _slice_audio_segment(self, audio_segment, slice_interval, allow_gaps=True):
         sliced_audio_segments = []
@@ -53,8 +61,8 @@ class DataPrep():
         return sliced_audio_segments
 
     def _export_audio_segments(self, audio_segments, directory_path, extension):
-        for index, sliced_audo_segment in enumerate(audio_segments):
-            sliced_audo_segment.export(
+        for index, sliced_audio_segment in enumerate(audio_segments):
+            sliced_audio_segment.export(
                 os.path.join(directory_path, '{filename}.{extension}'.format(filename=index, extension=extension)),
                 format=extension)
 
@@ -80,45 +88,45 @@ class DataPrep():
             return True
         return False
 
-    def slice_and_export(self, data_base_path, extension, slice_interval):
-        # data_base_path = '/Users/abhilash1in/Documents/Projects/Cookeroo/data/'
-        # extension = 'wav'
+    def export(self):
+        for category, audio_segments in self._sliced_audio_segments.items():
+            # path to the individual category sliced directory
+            category_data_sliced_directory_path = os.path.join(self._sliced_data_path, category)
+            # created the sliced directory if it does not exist
+            Path(category_data_sliced_directory_path).mkdir(parents=True, exist_ok=True)
+            # clear old data in sliced directory
+            self._clear_dir(category_data_sliced_directory_path)
+            # save sliced AudioSegments to sliced directory
+            self._export_audio_segments(audio_segments, category_data_sliced_directory_path, self._extension)
+
+    def slice(self, slice_interval):
         # slice_interval = 3 * 1000  # 3000 miliseconds = 3 seconds
 
-        raw_data_path = os.path.join(data_base_path, 'raw')
-        sliced_data_path = os.path.join(data_base_path, 'sliced')
-
         # raw > subdirectory names = category names
-        if not self._is_existing_dir(raw_data_path):
+        if not self._is_existing_dir(self._raw_data_path):
             raise ValueError(('Could not find \'raw\' directory at \'{raw_data_path}\'. '
                               'Place your audio data in subdirectories under \'raw\' directory '
-                              'and try again.').format(raw_data_path=raw_data_path))
+                              'and try again.').format(raw_data_path=self._raw_data_path))
 
         # raw > subdirectory names = category names
-        categories = next(os.walk(raw_data_path))[1]
+        categories = next(os.walk(self._raw_data_path))[1]
         # exclude hidden files
         categories = list(filter(lambda folder_name: not str(folder_name).startswith('.'), categories))
 
         if len(categories) == 0:
             raise ValueError(('Could not find subdirectories (for categories) under \'raw\' directory ({raw_data_path}). '
                               'Place your audio data in subdirectories (one subdirectory for each category) '
-                              'under \'raw\' directory and try again.').format(raw_data_path=raw_data_path))
+                              'under \'raw\' directory and try again.').format(raw_data_path=self._raw_data_path))
 
         for category in categories:
             # path to the individual category raw directory
-            category_data_raw_directory_path = os.path.join(raw_data_path, category)
-            # path to the individual category sliced directory
-            category_data_sliced_directory_path = os.path.join(sliced_data_path, category)
-            # created the sliced directory if it does not exist
-            Path(category_data_sliced_directory_path).mkdir(parents=True, exist_ok=True)
-            # clear old data in sliced directory
-            self._clear_dir(category_data_sliced_directory_path)
+            category_data_raw_directory_path = os.path.join(self._raw_data_path, category)
 
             # get list of file paths with given extension
-            category_data_raw_file_paths = self._get_file_paths(category_data_raw_directory_path, extension)
+            category_data_raw_file_paths = self._get_file_paths(category_data_raw_directory_path, self._extension)
             # list of AudioSegments (one for each file)
             audio_segments = self._get_audio_segments_wav(category_data_raw_file_paths)
             # list of all sliced AudioSegments combined
-            sliced_audo_segments = self._get_sliced_audio_segments(audio_segments, slice_interval)
-            # save sliced AudioSegments to sliced directory
-            self._export_audio_segments(sliced_audo_segments, category_data_sliced_directory_path, extension)
+            self._sliced_audio_segments[category] = self._get_sliced_audio_segments(audio_segments, slice_interval)
+
+        return self._sliced_audio_segments
